e80eb8e47ad3765d60d55794632f356cd030ac2c
n-meunier <nm.nicolasmeunier@gmail.com>
2020-10-19 12:28:45 +0900

    Add input read and test hidden word

--- bulls-and-cows/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ bulls-and-cows/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -3,9 +3,37 @@
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
+    HiddenWord = "computer";
+    PrintLine(TEXT("[DEBUG] The HiddenWord is: %s"), *HiddenWord);
+
+    Super::BeginPlay();
+    PrintLine(TEXT("Welcome to Bulls & Cows!"));
+    PrintLine(TEXT("Guess the %i letter word!"), HiddenWord.Len()); // Change the hardcoded number
+    PrintLine(TEXT("Type your guess and \nPress <Enter> to continue..."));
 
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    ClearScreen();
+
+    // Check if Input is an isogram
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("You found the Hidden Word! Well done!"));
+        PrintLine(Input);
+    } 
+    else
+    {
+        // Prompt to GuessAgain
+        PrintLine(TEXT("Wrong answer, try again!"));
+    }
+    
+        // Remove live
+        // Check if live < 0
+        // If yes GuessAgain
+        // If No Show GameOver and HiddenWord
+        // Prompt to play again
+        // Check User Input
+        // PlayAgain or Quit
 }

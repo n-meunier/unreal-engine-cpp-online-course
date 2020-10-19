789e780762816902c96d2d39c0b071108304b706
Nicolas Meunier <nicholas.meunier@ascent.ai>
2020-10-19 11:54:53 +0900

    Add Bulls and Cows UE4 start project

---
+++ bulls-and-cows/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "BullCowCartridge.h"
+
+void UBullCowCartridge::BeginPlay() // When the game starts
+{
+
+}
+
+void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+{
+}
